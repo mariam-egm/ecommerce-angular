18e5ff44ed90606bed9699f7c9b285dddf03791a
mariam-egm <mariam.gerges014@gmail.com>
2019-04-10 01:16:43 +0200

    login form and logging in

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      email:'',
+      password:''
+    })
+
+    this.loginForm.valueChanges.subscribe(console.log)
+  }
+  onSubmit(){
+    let localUsers: any;
+    let isMember: boolean= false;
+    if (localStorage.getItem("localUsers")) {
+      localUsers = JSON.parse(localStorage.getItem('localUsers'));
+    } else {
+        localUsers = [];
+    }
+    for(var i=0; i<localUsers.length; i++){
+      if(localUsers[i].email == this.loginForm.value.email)
+      {
+        isMember = true;
+        if(localUsers[i].password == this.loginForm.value.password)
+        {
+          console.log('you are logged in');
+        }else{
+          alert('Wrong user name or password');
+        }
+      }
+    }
+    if(isMember == false){
+      alert('You are not a member! Please register');
+    }  
   }
 
 }
