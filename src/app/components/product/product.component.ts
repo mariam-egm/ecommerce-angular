2ca5cb24e0c8ab5cfb87e6683e44ff062f9f4274
mariam-egm <mariam.gerges014@gmail.com>
2019-04-10 22:05:24 +0200

    home page with products

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
-
+  @Input('name') name: string;
+  @Input('price') price: string;
+  @Input('imageURL') imageURL: string;
+  @Input('description') description: string;
   ngOnInit() {
   }
 
