99dacdc210666f9751df6ee00ea9ed66b16eb9ad
mariam-egm <mariam.gerges014@gmail.com>
2019-04-12 21:45:16 +0200

    add local storage service

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -11,16 +11,21 @@ export class LocalStorageService {
   wishCounter: BehaviorSubject<any>= new BehaviorSubject('')
   constructor() { 
     this.cartCounter.next(this.getCartLength())
+    this.wishCounter.next(this.getWishLength())
   }
   getCartLength(){
     this.cartProducts = JSON.parse(localStorage.getItem('cartProducts')) || []
-    return this.cartProducts.length    
+    return this.cartProducts.length
   }
 
   getCartCounter(){
     return this.cartCounter.asObservable();
   }
-  // getWishList(){
-  //   this.wishListProducts
-  // }
+  getWishLength(){
+    this.wishListProducts = JSON.parse(localStorage.getItem('wishListProducts')) || []
+    return this.wishListProducts.length
+  }
+  getWishCounter(){
+    return this.wishCounter.asObservable();
+  }
 }
